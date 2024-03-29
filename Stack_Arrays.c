da527367da4ef53ca092a39bff5e16643c23780e
Vamsi1729 <142806600+Vamsi-Krisna-Ravilla@users.noreply.github.com>
2024-03-29 23:11:45 +0530

    Updated Stack_Arrays.c

--- Stack_Arrays.c
+++ Stack_Arrays.c
@@ -1,9 +1,15 @@
-//Stack Implementation using Arrays in C
+//All Stack Implementation using Arrays in C
 #include<stdio.h>
 #include<stdlib.h>
 # define MAX 5
 int stack[MAX];
 int top=-1;
+void display()
+{
+	int i;
+	printf("\nElements in Stack are:\t");
+	for(i=top;i>=0;i--) printf("%d\t",stack[i]);
+}
 void push()
 {
 	int value;
@@ -24,18 +30,29 @@ void pop()
 		top--;
 	}
 }
-void display()
+
+void peek() {
+    if (top == -1)
+        printf("\nStack is empty\n");
+    else
+        printf("\nTop element in stack is %d\n", stack[top]);
+}
+void isFull()
 {
-	int i;
-	printf("\nElements in Stack are:\t");
-	for(i=top;i>=0;i--) printf("%d\t",stack[i]);
+	if(top==MAX-1) printf("\nTRUE\n");
+	else printf("\nFALSE\n");
+}
+void isEmpty()
+{
+	if(top==-1) printf("\nTRUE\n");
+	else printf("\nFALSE\n");
 }
 void main()
 {
 	int choice;
 	while(1)
 	{
-		printf("\n1.exit\n2.display\n3.push\n4.pop\n");
+		printf("\n1.exit\n2.display\n3.push\n4.pop\n5.peek\n6.isFull\n7.isEMpty\n");
 		printf("Enter choice:\t");scanf("%d",&choice);
 		switch(choice)
 		{
@@ -43,6 +60,9 @@ void main()
 			case 2:display();break;
 			case 3: push();break;
 			case 4:pop();break;
+			case 5:peek();break;
+			case 6:isFull();break;
+			case 7:isEmpty();break;
 			default:printf("Invalid choice\n");
 		}
 	}
