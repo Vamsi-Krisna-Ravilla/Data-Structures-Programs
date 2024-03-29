6023d3d722e902b05c19fcfacb0d8942d26b87fb
Vamsi1729 <142806600+Vamsi-Krisna-Ravilla@users.noreply.github.com>
2024-03-29 14:54:06 +0530

    Updated reverseDLL() operation

--- Doubly Linked list.c
+++ Doubly Linked list.c
@@ -1,4 +1,3 @@
-//Various Operations of Doubly Linked list in C
 #include <stdio.h>
 #include <stdlib.h>
 struct node
@@ -98,16 +97,21 @@ void insertAtPosition()
     scanf("%d",&pos);
     
     if(pos <= 0 ) printf("Enter correct position for Insertion");
+    else if(pos==1) insertBegin();
     else
     {
-        struct node *newNode = (struct node *) malloc(sizeof(struct node));
-    	printf("Enter elem value: ");scanf("%d",&value);
-    	newNode->data=value;
         for(i=1;i<pos-1;i++) {temp=temp->next;}
-        newNode->next=temp->next; 
-        temp->next=newNode; newNode->prev=temp;
-        temp=newNode->next;temp->prev=newNode;
-        printf("Inserted element %d at position %d of LL",value,pos);
+        if(temp->next==NULL) insertEnd();
+        else
+		{
+			struct node *newNode = (struct node *) malloc(sizeof(struct node));
+	    	printf("Enter elem value: ");scanf("%d",&value);
+	    	newNode->data=value;
+	        newNode->next=temp->next; 
+	        temp->next=newNode; newNode->prev=temp;
+	        temp=newNode->next;temp->prev=newNode;
+	        printf("Inserted element %d at position %d of LL",value,pos);
+	    }
     }
 }
 void deleteAtPosition()
@@ -117,14 +121,20 @@ void deleteAtPosition()
     printf("Enter position to delete node:\n");
     scanf("%d",&pos);
     if(pos <= 0 ) printf("Deletion not possible");
+    else if(pos==1) deleteBegin();
     else
     {
         for(i=1;i<pos-1;i++) temp=temp->next;
-        //struct node *del=temp->next;
-        printf("Element : %d got deleted at position %d",temp->next->data,pos);
-        temp->next=temp->next->next;temp->next->prev=temp;
-        
-        //temp->next=del->next;free(del);del=temp->next;del->prev=temp;
+        if(temp->next==NULL) deleteEnd();
+        else
+        {
+	        //struct node *del=temp->next;
+	        printf("Element : %d got deleted at position %d",temp->next->data,pos);
+	        temp->next=temp->next->next;
+			if(temp->next!=NULL)temp->next->prev=temp;
+	        
+	        //temp->next=del->next;free(del);del=temp->next;del->prev=temp;
+	    }
     }
 }
 void reverseDisplay()
@@ -134,17 +144,38 @@ void reverseDisplay()
 	{
 		struct node *temp=tail;
 		printf("\n Displaying DLL elements in Reverse order\n");
-		while(temp!=NULL)
+		while(temp!=head)
 		{
 			printf("%d\t",temp->data); temp=temp->prev;
 		}
+		printf("%d\t",head->data);
 	}
+}
+void reverseDLL() 
+{
+    struct node *temp = NULL, *current = head;
+    while (current != NULL) 
+    {
+        // Swap prev and next pointers
+        temp = current->prev;
+        current->prev = current->next;
+        current->next = temp;
+        
+        // Move forward
+        current = current->prev;
+    }
+    
+    // Update head and tail pointers after reversal
+    if (temp != NULL) {
+        tail = head; // Tail becomes the previous head, which is now the last node
+        head = temp->prev; // Head becomes the previous node of the current (last) node
+    }
 }
 void main() {
     int choice;
     while(1)
     {
-        printf("\n 1.Exit\n2.Display\n3.InsertEnd\n4.InserBegin\n5.DeleteEnd\n6.DeleteBegin\n7.InsertAtPosition\n8.DeleteAtPosition\n9.ReverseDisplay\n");
+        printf("\n 1.Exit\n2.Display\n3.InsertEnd\n4.InserBegin\n5.DeleteEnd\n6.DeleteBegin\n7.InsertAtPosition\n8.DeleteAtPosition\n9.ReverseDLL\n");
     
         printf("\nEnter Choice\n");scanf("%d",&choice);
         switch(choice)
@@ -157,7 +188,7 @@ void main() {
             case 6: deleteBegin();break;
             case 7: insertAtPosition();break;
             case 8: deleteAtPosition();break;
-            case 9: reverseDisplay();break;
+            case 9: reverseDLL();break;
         }
     }
 }
